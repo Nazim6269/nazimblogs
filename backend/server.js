79905ded49f583c5147012abc595dc49811dacac
Nazim6269 <nazimdev10022001@gmail.com>
2026-01-20 13:18:17 +0600

    ♻️ refactor: restructure into monorepo with workspaces

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,13 @@
+
 import express from 'express';
-import dotenv from 'dotenv';
+
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import connectDB from './config/db.js';
 import userRoutes from './routes/userRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import { port } from './secret.js';
 
-dotenv.config();
 
 connectDB();
 
@@ -22,13 +23,11 @@ app.use(cookieParser());
 
 app.use('/api/users', userRoutes);
 
-if (process.env.NODE_ENV === 'production') {
-    // Set static folder
-} else {
-    app.get('/', (req, res) => {
-        res.send('API is running Smoothly');
-    });
-}
+
+app.get('/', (req, res) => {
+    res.send('API is running Smoothly');
+});
+
 
 // Error Middleware
 app.use(notFound);
@@ -37,5 +36,5 @@ app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port http://localhost:${port}`);
 });
